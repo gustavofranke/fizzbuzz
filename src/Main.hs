c15b2abe09cd02ce5e848bc5884833117a4abe18
Gustavo Franke <franke.gustavo@gmail.com>
2020-11-26 15:47:16 +0000

    apply hlint suggestion and use guards

--- src/Main.hs
+++ src/Main.hs
@@ -8,15 +8,11 @@ fizzBuzz :: [Integer] -> [String]
 fizzBuzz = map fizzLogic
 
 fizzLogic :: Integer -> String
-fizzLogic i =
-    if i `mod` 3 == 0 && i `mod` 5 == 0 then
-        "FizzBuzz"
-    else if i `mod` 3 == 0 then
-        "Fizz"
-    else if i `mod` 5 == 0 then
-        "Buzz"
-    else
-        show i
+fizzLogic i
+  | i `mod` 3 == 0 && i `mod` 5 == 0 = "FizzBuzz"
+  | i `mod` 3 == 0                   = "Fizz"
+  | i `mod` 5 == 0                   = "Buzz"
+  | otherwise                        = show i
 
 main :: IO ()
 main = mapM_ print (fizzBuzz [1..100])
