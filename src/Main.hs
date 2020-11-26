b3a9446ae8f0645b79d86931d8ad8dbb3cb6ff96
Gustavo Franke <franke.gustavo@gmail.com>
2020-11-26 15:44:35 +0000

    extract print effect out

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,22 @@
 module Main where
 
-import Control.Monad ( forM_ )
-
 -- | Print the numbers 1 to 100.
--- If the number is a multiple of 3, then print “Fizz” instead of the number.
--- If the number is a multiple of 5, then print “Buzz” instead of the number.
--- If the number is divisible by both 3 and 5, then print “FizzBuzz” instead.
-fizzBuzz :: IO ()
-fizzBuzz =
-    forM_ [1..100] (\i ->
-        if i `mod` 3 == 0 && i `mod` 5 == 0 then
-            putStrLn "FizzBuzz"
-        else if i `mod` 3 == 0 then
-            putStrLn "Fizz"
-        else if i `mod` 5 == 0 then
-            putStrLn "Buzz"
-        else
-            print i
-    )
+-- If the number is a multiple of 3, then print "Fizz" instead of the number.
+-- If the number is a multiple of 5, then print "Buzz" instead of the number.
+-- If the number is divisible by both 3 and 5, then print "FizzBuzz" instead.
+fizzBuzz :: [Integer] -> [String]
+fizzBuzz = map fizzLogic
+
+fizzLogic :: Integer -> String
+fizzLogic i =
+    if i `mod` 3 == 0 && i `mod` 5 == 0 then
+        "FizzBuzz"
+    else if i `mod` 3 == 0 then
+        "Fizz"
+    else if i `mod` 5 == 0 then
+        "Buzz"
+    else
+        show i
 
 main :: IO ()
-main = fizzBuzz
+main = mapM_ print (fizzBuzz [1..100])
