bde410aa4df19c6a0bc10c1f39e24ee672951d01
Gustavo Franke <franke.gustavo@gmail.com>
2020-11-26 14:25:18 +0000

    add naive solution

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,23 @@
 module Main where
 
+import Control.Monad ( forM_ )
+
+-- | Print the numbers 1 to 100.
+-- If the number is a multiple of 3, then print “Fizz” instead of the number.
+-- If the number is a multiple of 5, then print “Buzz” instead of the number.
+-- If the number is divisible by both 3 and 5, then print “FizzBuzz” instead.
+fizzBuzz :: IO ()
+fizzBuzz =
+    forM_ [1..100] (\i ->
+        if i `mod` 3 == 0 && i `mod` 5 == 0 then
+            putStrLn "FizzBuzz"
+        else if i `mod` 3 == 0 then
+            putStrLn "Fizz"
+        else if i `mod` 5 == 0 then
+            putStrLn "Buzz"
+        else
+            print i
+    )
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = fizzBuzz
